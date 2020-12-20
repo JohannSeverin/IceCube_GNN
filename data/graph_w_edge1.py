020f648cda872eda9128eaf68550e445f98e8ca7
Johann <JohannSeverin@github.com>
2020-12-20 17:18:45 +0100

    graph 6 neighbors with edge attributes dataset

---
+++ data/graph_w_edge1.py
@@ -0,0 +1,152 @@
+import numpy as np
+import os, sqlite3, pickle, sys, gzip, shutil, time
+from tqdm import tqdm
+import os.path as osp
+
+from pandas import read_sql, concat
+from sklearn.preprocessing import normalize, RobustScaler
+from sklearn.neighbors import kneighbors_graph as knn
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+from spektral.data import Dataset, Graph
+
+
+class graph_w_edge1(Dataset):
+    """
+    Class for a graph dataset with the following edge features:
+    distance, time-difference and unit vector for direction
+    The node features are normalized with a robust scaler before calculating edge features
+    """
+
+    def __init__(self, n_data = None, n_neighbors = 6, **kwargs):
+        self.n_data = n_data
+        self.n_neighbors = n_neighbors
+        super().__init__(**kwargs)
+
+    @property
+    def path(self):
+        """
+        Set the path of the data to be in the processed folder
+        """
+        cwd = osp.dirname(osp.realpath(__file__))
+        path = osp.join(cwd, "processed/graph_w_edge1")
+        return path
+
+    def download(self):
+        download_start = time.time()
+        # Get raw_data
+        db_folder = osp.join(osp.dirname(osp.realpath(__file__)), "raw_files")
+        db_list   = os.listdir(db_folder)
+
+        # Make output folder
+        os.mkdir(self.path )
+
+        # Create data loop 
+        downloaded = 0
+        seq, sca = None, None
+        if self.n_data == None:
+            self.n_data = 999999999999
+        print("Starting data generation")
+        for file in db_list:
+            with sqlite3.connect(osp.join(db_folder, file)) as con:
+                print(f"Reading {file}")
+                distinct = read_sql(f"select distinct event_no from sequential", con).event_no
+                if len(distinct) < self.n_data - downloaded:
+                    limit = self.n_data - downloaded
+                else:
+                    limit = distinct[self.n_data - downloaded]
+                if not seq:
+                    seq     = read_sql(f"select * from sequential where event_no < {limit};", con)
+                    sca     = read_sql(f"select * from scalar where event_no < {limit};", con)
+                else:
+                    seq     = concat(seq, read_sql(f"select * from sequential where event_no < {limit};", con))
+                    sca     = concat(sca, read_sql(f"select * from scalar where event_no < {limit};", con))
+            downloaded = len(seq.event_no.unique())
+            if downloaded >= self.n_data:
+                print(f"Succesfully loaded data for {n_data} graphs")
+                break
+            if file == db_list[-1]:
+                print(f"All raw  data loaded: {downloaded} graphs")
+        
+        # Set attributes
+        node_cols = ["dom_x", "dom_y", "dom_z", "dom_charge", "dom_time"]
+        event_no  = seq.event_no.unique()[sca.event_no.isin(sca.event_no)]
+        seq       = seq[seq.event_no.isin(event_no)]
+        sca       = sca[sca.event_no.isin(event_no)]
+        targets   = ["true_primary_energy"]
+
+        idx_nodes = np.array(seq.event_no)
+        idx_targ  = np.array(sca.event_no)
+
+        nodes_arr = np.array(seq.loc[:, node_cols])
+        targ_arr  = np.array(sca.loc[:, targets])
+
+        nodes_arr  = RobustScaler().fit_transform(nodes_arr)
+        targ_arr  = RobustScaler().fit_transform(targ_arr)
+
+        # Find cuts for node_arr
+        ids, start, count = np.unique(idx_nodes, return_index = True, return_counts = True)
+
+        graph_list = []
+
+        for id, s, c in tqdm(zip(ids, start, count), total = len(ids)):
+            x = nodes_arr[s: s + c + 1 , :]
+            y = targ_arr[idx_targ == id]
+
+            a, e = calculate_edge_attributes(x, n_neighbors = self.n_neighbors)
+
+            graph_list.append((x, a, e, y))
+
+
+        print("Saving file")
+        start_time = time.time()
+        with open(osp.join(self.path, "graph_w_edge1.dat"), 'wb') as file:
+            pickle.dump(graph_list, file)
+        print(f"Data saved in {time.time() - start_time:.1f} seconds")
+        print(f"Total time to create dataset: {time.time() - download_start:.1f} seconds")
+
+    def read(self):
+        file_path = osp.join(self.path, "graph_w_edge1.dat")
+        with open(file_path, 'rb') as file:
+            graph_list = pickle.load(file)
+        
+        print("Loading data to memory")
+        output = []
+        for tup in graph_list:
+            x, a, e, y = tup
+            output.append(Graph(x, a, e, y))
+        return output
+        
+
+
+
+def calculate_edge_attributes(x, n_neighbors):
+    pos, charge, time = x[:, :3], x[:, 3], x[:, 4]
+    a = knn(x[:, :3], n_neighbors)
+
+    index_i, index_j = np.repeat(np.arange(a.shape[0]), n_neighbors), a.indices
+
+    dists = np.linalg.norm(pos[index_i] - pos[index_j], axis = 1)
+    vects = normalize(pos[index_i] - pos[index_j])
+    dts = time[index_j] - time[index_i]
+
+    e = np.vstack(np.vstack([dists, dts, vects.T])).T
+
+    return a, e
+
+
+
+if __name__ == "__main__":
+    print("Removing current data folder")
+    if os.path.isdir(osp.join(osp.dirname(osp.realpath(__file__)), "processed", "graph_w_edge1")):
+        shutil.rmtree(osp.join(osp.dirname(osp.realpath(__file__)), "processed", "graph_w_edge1"))
+    if len(sys.argv) == 2:
+        n_data = int(sys.argv[1])
+        print(f"Preparing dataset with {n_data} graphs")
+    else:
+        n_data = None
+        print("Preparing dataset with all availible raw data")
+
+    # Preparing data 
+    dataset = graph_w_edge1(n_data)
